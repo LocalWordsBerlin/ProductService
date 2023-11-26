66ec3fd12c942bb373630f2f91ff7ee678916963
hannah-schmidt <hannah.schmidt@posteo.de>
2023-11-27 00:34:23 +0100

    spring boot setup

---
+++ src/test/java/htw/ai/softwarearchitekturen/LocalWords/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.ai.softwarearchitekturen.LocalWords.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
