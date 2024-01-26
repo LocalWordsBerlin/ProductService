d4e59d31f5edc079a41526cfd6b69c410a2a1c19
hannah-schmidt <hannah.schmidt@posteo.de>
2024-01-26 14:17:51 +0100

    warehouse communication

--- src/main/java/htw/ai/softwarearchitekturen/LocalWords/ProductService/port/consumer/ProductStockConsumer.java
+++ src/main/java/htw/ai/softwarearchitekturen/LocalWords/ProductService/port/consumer/ProductStockConsumer.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductStockConsumer {
-
+/**
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductStockConsumer.class);
 
-    @RabbitListener(queues = "userQueue")
+    @RabbitListener(queues = "stockQueue")
     public void consume(String message) {
         LOGGER.info(String.format("Received message -> %s ", message));
         //add Logic to interpret message and add or remove STOCK of Product
     }
+**/
 }
